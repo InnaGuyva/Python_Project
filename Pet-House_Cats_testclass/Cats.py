c9a3c912e4cba539496766a95de1595dceaaa749
[InnaGuyva] <[innasamos1978@gmail.com]>
2024-06-01 19:23:52 +0300

    home work

---
+++ Pet-House_Cats_testclass/Cats.py
@@ -0,0 +1,5 @@
+from parametrs import Cats
+cat_1 = Cats("Байрон", "мальчик", "2")
+cat_2 = Cats("Сэм", "мальчик", "2")
+print ("Первый кот (cat_1):", cat_1)
+print ("Второй кот (cat_2):", cat_2)
---
+++ Pet-House_Cats_testclass/parametrs.py
@@ -0,0 +1,13 @@
+class Cats:
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
+    def getName(self):
+        return self.name
+    def getGender(self):
+        return self.gender
+    def getAge(self):
+        return self.age
+    def __str__(self):
+        return f'Имя {self.name}, пол {self.gender}, возраст {self.age}'
